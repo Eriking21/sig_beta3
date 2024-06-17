27a9bc242e54d9fecbc0882e3704fbde9389ffdc
Eriking21 <64511659+Eriking21@users.noreply.github.com>
2024-06-17 14:22:34 +0100

    Update RunServer.bat

--- RunServer.bat
+++ RunServer.bat
@@ -1,6 +1,10 @@
 @echo off
+cd ..
 
-git pull origin main 
-pnpm run dev
+rm rmdir /s /q sig_beta3 2>nul
+git clone https://github.com/Eriking21/sig_danny
+cd sig_danny
+npm update
+npm run dev
 
 @echo server closed
